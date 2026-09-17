70208032e49c50a138bc56a043a7062b2501a628
abhishekchariii <abhitopup@gmail.com>
2026-09-17 16:39:30 +0530

    Allow Vercel frontend CORS

--- erp-backend/src/main.ts
+++ erp-backend/src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
 
   // Enable CORS for React frontend
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin:'https://tech-m4-india-erp-04dx.vercel.app',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     allowedHeaders: 'Content-Type, Authorization',
   });
